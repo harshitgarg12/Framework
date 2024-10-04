28ab24cb7522ba846d307631f73da474823c1a86
Harshit Garg <harshitgargbm@gmail.com>
2024-10-04 12:26:36 +0530

    E2E modified

--- src/test/java/testcases/E2ETest.java
+++ src/test/java/testcases/E2ETest.java
@@ -14,6 +14,7 @@ import pages.ManageCourses;
 
 public class E2ETest extends BaseClass   {
 
+	//Test1
 	@Test(dataProvider = "loginDetails", dataProviderClass = CustomDataProvider.class, priority = 1)
 	public void LoginToApplication(String email, String password)
 	{
